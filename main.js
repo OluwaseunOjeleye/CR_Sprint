f8e8ea7a3f826689598d4781e59332175ed051cc
Oluwaseun Ojeleye <oluwaseunojeleye@pm.me>
2022-06-09 17:07:31 +0200

    update

--- main.js
+++ main.js
@@ -11,6 +11,19 @@ var WIDTH = 1940, HEIGHT = 1080;
 // Image and Video Textures
 var imageProcessing, imageProcessingMaterial;
 
+const LIP_MESH = [61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95, 78,
+	61, 185, 40, 39, 37, 0, 267, 269, 270, 409, 291, 308, 415, 310, 311, 312, 13, 82, 81, 80, 191, 78
+	];
+
+const RIGHTEYE_MESH = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161, 246];
+const LEFTEYE_MESH = [362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398];
+
+const RIGHTEYEBROW_MESH = [46, 53, 52, 65, 55, 107, 66, 105, 63, 70];
+const LEFTEYEBROW_MESH = [285, 295, 282, 283, 276, 300, 293, 334, 296, 336];
+
+const RIGHTIRIS_MESH = [469, 470, 471, 472];
+const LEFTIRIS_MESH = [474, 475, 476, 477];
+
 // Face Mesh
 var faceMesh, webcam;
 var LeftEye_Vertices = [], RightEye_Vertices = [],
@@ -51,26 +64,29 @@ function image_Processing(){
 
 			show_env:{type: "b", value: true},
 
-			RightEye_Vertices: {value: RightEye_Vertices},
-			LeftEye_Vertices: {value: LeftEye_Vertices},
+			no_faces: {type: 'i', value: 1},
 
-			RightIris_Vertices: {value: RightIris_Vertices},
-			LeftIris_Vertices: {value: LeftIris_Vertices},
+			RightEye_Vertices: {type: 't', value: null},
+			LeftEye_Vertices: {type: 't', value: null},
 
-			LeftEyebrow_Vertices: {value: LeftEyebrow_Vertices},
-			RightEyebrow_Vertices: {value: RightEyebrow_Vertices},
+			RightIris_Vertices: {type: 't', value: null},
+			LeftIris_Vertices: {type: 't', value: null},
 
-			Lips_Vertices: {value: Lips_Vertices},
+			LeftEyebrow_Vertices: {type: 't', value: null},
+			RightEyebrow_Vertices: {type: 't', value: null},
+			eyebrowcolor:{value: new THREE.Color('#6093a4')},
+
+			Lips_Vertices: {type: 't', value: null},
 			Face_Vertices: {value: Face_Vertices},
     	},
 		defines: {
 			MAX_SIZE_EYE: 32,
-			MAX_SIZE_IRIS: 8,
+			MAX_SIZE_IRIS: 120, //8,
 			MAX_SIZE_EYEBROW: 20,
 			MAX_SIZE_LIPS: 88,
 			MAX_SIZE_FACE: 74,
 
-			MAX_SIZE: 100,
+			MAX_SIZE: 120,
 		},
 		
     	vertexShader: document.getElementById('vertShader').text,
@@ -88,119 +104,129 @@ function image_Processing(){
     scene.add(processed_image);
 }
 
-function get_Meshes(landmarks, STRUCTURE){
-	let i = 0, Vertices = [];
-	for(; i < STRUCTURE.length; i++){
-		Vertices.push(landmarks[STRUCTURE[i][0]].x * WIDTH);
-		Vertices.push((1 - landmarks[STRUCTURE[i][0]].y) * HEIGHT);
-	}
-	Vertices.push((landmarks[STRUCTURE[STRUCTURE.length - 1][1]].x) * WIDTH);
-	Vertices.push((1 - landmarks[STRUCTURE[STRUCTURE.length - 1][1]].y) * HEIGHT);
-	return Vertices;
-}
-
-
 function get_Mesh_Vertices(landmarks, list){
-	var i = 0, Vertices = [];
-	for(; i < list.length; i++){
+	var Vertices = [];
+	for(var i = 0; i < list.length; i++){
 		Vertices.push(landmarks[list[i]].x * WIDTH);
 		Vertices.push((1 - landmarks[list[i]].y) * HEIGHT);
 	}	
 	return Vertices;
 }
 
-// Face Mesh Prediction and Webcam
-const LIP_MESH = [61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95, 78,
-					61, 185, 40, 39, 37, 0, 267, 269, 270, 409, 291, 308, 415, 310, 311, 312, 13, 82, 81, 80, 191, 78
-					];
-
-const RIGHTEYE_MESH = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161, 246];
-const LEFTEYE_MESH = [362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398];
-
-const RIGHTEYEBROW_MESH = [46, 53, 52, 65, 55, 107, 66, 105, 63, 70];
-const LEFTEYEBROW_MESH = [285, 295, 282, 283, 276, 300, 293, 334, 296, 336];
-
-const RIGHTIRIS_MESH = [469, 470, 471, 472];
-const LEFTIRIS_MESH = [474, 475, 476, 477];
-
-
 function get_iris_Vertices(Vertices){
 	var newVertices = [];
-	var x = (Vertices[2] - Vertices[0]) / 2;
-	var y = (Vertices[3] - Vertices[1]) / 2;
+	var x = (Vertices[4] + Vertices[0]) / 2;
+	var y = (Vertices[5] + Vertices[1]) / 2;
 	var r = Math.sqrt(Math.pow((x - Vertices[0]), 2) + Math.pow((y - Vertices[1]), 2));
 
 	for(var theta = 0; theta < 360; theta += 6){
-		newVertices.push(r * Math.cos(theta * Math.PI / 180));
-		newVertices.push(r * Math.sin(theta *Math.PI / 180));
+		newVertices.push(x + r * Math.sin(theta * Math.PI / 180));
+		newVertices.push(y + r * Math.cos(theta * Math.PI / 180));
 	}
+	// console.log(Vertices)
 	return newVertices;
 }
 
+function init_Structures_Vertices(){
+	RightEye_Vertices = [];
+	LeftEye_Vertices = [];
+	RightIris_Vertices = [];
+	LeftIris_Vertices = [];
+	LeftEyebrow_Vertices = [];
+	RightEyebrow_Vertices = [];
+	Lips_Vertices = [];
+	Face_Vertices = [];
+}
 
 function onResults(results) {
+	//console.log(results.multiFaceLandmarks.length);
+
 	canvasCtx.save();
 	canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-	canvasCtx.drawImage(
-		results.image, 0, 0, canvasElement.width, canvasElement.height);
+	canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
 	if (results.multiFaceLandmarks) {
-	  for (const landmarks of results.multiFaceLandmarks) {
+		init_Structures_Vertices();
 		
-		RightEye_Vertices = get_Mesh_Vertices(landmarks, RIGHTEYE_MESH);
-		LeftEye_Vertices = get_Mesh_Vertices(landmarks, LEFTEYE_MESH);
+	  	for (var i = 0 ; i < results.multiFaceLandmarks.length; i++) {
+			RightEye_Vertices.push(get_Mesh_Vertices(results.multiFaceLandmarks[i], RIGHTEYE_MESH));
+			LeftEye_Vertices.push(get_Mesh_Vertices(results.multiFaceLandmarks[i], LEFTEYE_MESH));
 
-		// var right = get_iris_Vertices(get_Mesh_Vertices(landmarks, RIGHTIRIS_MESH));
-		// RightIris_Vertices = get_iris_Vertices(get_Mesh_Vertices(landmarks, RIGHTIRIS_MESH));
-		// LeftIris_Vertices = get_iris_Vertices(get_Mesh_Vertices(landmarks, LEFTIRIS_MESH));
-		
-		RightIris_Vertices = get_Mesh_Vertices(landmarks, RIGHTIRIS_MESH);
-		LeftIris_Vertices = get_Mesh_Vertices(landmarks, LEFTIRIS_MESH);
+			// var right = get_iris_Vertices(get_Mesh_Vertices(landmarks, RIGHTIRIS_MESH));
+			RightIris_Vertices.push(get_iris_Vertices(get_Mesh_Vertices(results.multiFaceLandmarks[i], RIGHTIRIS_MESH)));
+			LeftIris_Vertices.push(get_iris_Vertices(get_Mesh_Vertices(results.multiFaceLandmarks[i], LEFTIRIS_MESH)));
+			
+			// RightIris_Vertices.push(get_Mesh_Vertices(results.multiFaceLandmarks[i], RIGHTIRIS_MESH));
+			// LeftIris_Vertices.push(get_Mesh_Vertices(results.multiFaceLandmarks[i], LEFTIRIS_MESH));
 
-		LeftEyebrow_Vertices = get_Mesh_Vertices(landmarks, LEFTEYEBROW_MESH);
-		RightEyebrow_Vertices = get_Mesh_Vertices(landmarks, RIGHTEYEBROW_MESH);
+			LeftEyebrow_Vertices.push(get_Mesh_Vertices(results.multiFaceLandmarks[i], LEFTEYEBROW_MESH));
+			RightEyebrow_Vertices.push(get_Mesh_Vertices(results.multiFaceLandmarks[i], RIGHTEYEBROW_MESH));
 
-		Lips_Vertices = get_Mesh_Vertices(landmarks, LIP_MESH);
-		Face_Vertices = get_Meshes(landmarks, FACEMESH_FACE_OVAL);
+			Lips_Vertices.push(get_Mesh_Vertices(results.multiFaceLandmarks[i], LIP_MESH));
+			// Face_Vertices.push(get_Meshes(results.multiFaceLandmarks[i], FACEMESH_FACE_OVAL));
 
-		//console.log(FACEMESH_LEFT_IRIS);
-		// TODO: FACEMESH_TESSELATION - For smiling or different colors in different triangles
-
-	  }
+			// TODO: FACEMESH_TESSELATION - For smiling or different colors in different triangles
+	  	}
 	}
 	canvasCtx.restore();
 
+	// console.log(RightEye_Vertices.flat());
+
+	// Creating Textures
 	const texture = new THREE.CanvasTexture(canvasCtx.canvas);
 
 	texture.minFilter = THREE.NearestFilter;
 	texture.magFilter = THREE.NearestFilter;
-	texture.generateMipmaps = false; 
+	texture.generateMipmaps = false;
 	texture.format = THREE.RGBFormat;
 
+	var tex_RightEye_Vertices = new THREE.DataTexture(new Float32Array(RightEye_Vertices.flat()), RightEye_Vertices.flat().length, 1, THREE.RedFormat,THREE.FloatType);
+	var tex_LeftEye_Vertices = new THREE.DataTexture(new Float32Array(LeftEye_Vertices.flat()), LeftEye_Vertices.flat().length, 1, THREE.RedFormat,THREE.FloatType);
+
+	var tex_RightIris_Vertices = new THREE.DataTexture(new Float32Array(RightIris_Vertices.flat()), RightIris_Vertices.flat().length, 1, THREE.RedFormat,THREE.FloatType);
+	var tex_LeftIris_Vertices = new THREE.DataTexture(new Float32Array(LeftIris_Vertices.flat()), LeftIris_Vertices.flat().length, 1, THREE.RedFormat,THREE.FloatType);
+
+	var tex_RightEyebrow_Vertices = new THREE.DataTexture(new Float32Array(RightEyebrow_Vertices.flat()), RightEyebrow_Vertices.flat().length, 1, THREE.RedFormat,THREE.FloatType);
+	var tex_LeftEyebrow_Vertices = new THREE.DataTexture(new Float32Array(LeftEyebrow_Vertices.flat()), LeftEyebrow_Vertices.flat().length, 1, THREE.RedFormat,THREE.FloatType);
+
+	var tex_Lips_Vertices = new THREE.DataTexture(new Float32Array(Lips_Vertices.flat()), Lips_Vertices.flat().length, 1, THREE.RedFormat,THREE.FloatType);
+
+	
+	// Update Textures
+	tex_RightEye_Vertices.needsUpdate = true;
+	tex_LeftEye_Vertices.needsUpdate = true;
+	tex_RightIris_Vertices.needsUpdate = true;
+	tex_LeftIris_Vertices.needsUpdate = true;
+	tex_RightEyebrow_Vertices.needsUpdate = true;
+	tex_LeftEyebrow_Vertices.needsUpdate = true;	
+	tex_Lips_Vertices.needsUpdate = true;
+
+
+	// Update Uniforms
 	imageProcessingMaterial.uniforms.image.value = texture;
+	imageProcessingMaterial.uniforms.no_faces.value = results.multiFaceLandmarks.length;
 
-	imageProcessingMaterial.uniforms.RightEye_Vertices.value = RightEye_Vertices;
-	imageProcessingMaterial.uniforms.LeftEye_Vertices.value = LeftEye_Vertices;
+	imageProcessingMaterial.uniforms.RightEye_Vertices.value = tex_RightEye_Vertices;
+	imageProcessingMaterial.uniforms.LeftEye_Vertices.value = tex_LeftEye_Vertices;
 
-	imageProcessingMaterial.uniforms.RightIris_Vertices.value = RightIris_Vertices;
-	imageProcessingMaterial.uniforms.LeftIris_Vertices.value = LeftIris_Vertices;
+	imageProcessingMaterial.uniforms.RightIris_Vertices.value = tex_RightIris_Vertices;
+	imageProcessingMaterial.uniforms.LeftIris_Vertices.value = tex_LeftIris_Vertices;
 
-	imageProcessingMaterial.uniforms.RightIris_Vertices.value = RightIris_Vertices;
-	imageProcessingMaterial.uniforms.LeftIris_Vertices.value = LeftIris_Vertices;
+	imageProcessingMaterial.uniforms.RightEyebrow_Vertices.value = tex_RightEyebrow_Vertices;
+	imageProcessingMaterial.uniforms.LeftEyebrow_Vertices.value = tex_LeftEyebrow_Vertices;
 
-	imageProcessingMaterial.uniforms.RightEyebrow_Vertices.value = RightEyebrow_Vertices;
-	imageProcessingMaterial.uniforms.LeftEyebrow_Vertices.value = LeftEyebrow_Vertices;
+	imageProcessingMaterial.uniforms.Lips_Vertices.value = tex_Lips_Vertices;
+	// imageProcessingMaterial.uniforms.Face_Vertices.value = Face_Vertices.flat();
 
-	imageProcessingMaterial.uniforms.Lips_Vertices.value = Lips_Vertices;
-	imageProcessingMaterial.uniforms.Face_Vertices.value = Face_Vertices;
 
 }
 
+// Face Mesh Prediction and Webcam
 function face_Mesh(){
 	faceMesh = new FaceMesh({locateFile: (file) => {
 		return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
 	}});
 	faceMesh.setOptions({
-		maxNumFaces: 1,
+		maxNumFaces: 3,
 		refineLandmarks: true,
 		minDetectionConfidence: 0.5,
 		minTrackingConfidence: 0.5
@@ -268,17 +294,19 @@ function init () {
 	const parameters = {
 		model: 0,
 		env: true,
+		eyebrowcolor: "#6093a4",
 	};
 
 	function update() {
 		imageProcessingMaterial.uniforms.type.value = parameters.model;
 		imageProcessingMaterial.uniforms.show_env.value = parameters.env;
-
+		imageProcessingMaterial.uniforms.eyebrowcolor.value = new THREE.Color(parameters.eyebrowcolor);
 	}
 	
 	gui = new GUI();
 	gui.add(parameters, 'model', { "Clown": 0, "Blue": 1, "Alien": 2}).onChange( update );
-	gui.add(parameters, 'env').name("Environment").onChange( update );;
+	gui.add(parameters, 'env').name("Environment").onChange( update );
+	gui.addColor(parameters, 'eyebrowcolor').name("Eye Brow").onChange( update );
 
 	window.addEventListener( 'resize', onWindowResize, false );
 }
